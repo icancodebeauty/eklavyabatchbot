bd2b97ba3a936e4639fa3d25c7bd47d7f6889d21
Aditya Yadav <adi@mail.io>
2022-05-07 22:22:28 +0530

    Moved To Telegraf

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 /* Importing All Dependencies */
-require('dotenv').config();
-const TB = require('node-telegram-bot-api');
+const { Telegraf } = require('telegraf');
 const fetch = require("cross-fetch");
+require('dotenv').config();
 /* Creating A Bot With Token */
-const bot = new TB(process.env.TOKEN, { polling: true });
-/* Message */
-bot.on('message', async (message) => {
-    if (!message.text) { return } else {
-        if (message.text.startsWith("/start")) {
-          bot.sendMessage(message.chat.id, `<b>List Of Commands</b>\n\n• /today : Gives Todays Mock Test Details\n• /links : Gives All Social Media Links`, {parse_mode : "HTML"})
-        } else if (message.text.startsWith("/today")) {
-          let response = await fetch("https://eklavya.deta.dev/today")
-          let data = await response.json()
-          if(data.key == "0"){
-            bot.sendMessage(message.chat.id, `<b>No Mock Tests Today!!</b>`, {parse_mode : "HTML"})
-          } else {
-            bot.sendMessage(message.chat.id, `<b>Today's Mock Test Details</b>\n\n• Subject : ${data.subject}\n• Chapter : ${data.subject}`, {parse_mode : "HTML"})
-          }
-        } else if (message.text.startsWith("/links")) {
-          let response = await fetch("https://eklavya.deta.dev/socials")
-          let data = await response.json()
-          bot.sendMessage(message.chat.id, `<b>All Links</b>\n\n• Instagram : ${data.instagram}\n• Telegram : ${data.telegram}\n• Website : ${data.website}\n`, {parse_mode : "HTML"})
-        }
-    };
-});
+const bot = new Telegraf(process.env.TOKEN);
+/* Starting Bot */
+bot.launch()
+/* /start Commands */
+bot.start((ctx) => {
+  ctx.telegram.sendChatAction(ctx.chat.id, "typing")
+  ctx.reply(`<b>List Of Commands</b>
+• /today - Gives Todays Mock Test Details
+• /links - Gives All Social Media Links`,
+  {parse_mode : "HTML"})
+})
+/* /today Command */
+bot.command("today", async (ctx) => {
+  ctx.telegram.sendChatAction(ctx.chat.id, "typing")
+  let response = await fetch("https://eklavya.deta.dev/today")
+  let data = await response.json()
+  if(data.key == "0"){
+    ctx.reply(`<b>No Mock Tests Today!!</b>`, {parse_mode : "HTML"})
+  } else {
+    ctx.reply(`<b>Today's Mock Test Details</b>
+• Subject : ${data.subject}
+• Chapter : ${data.chapter}`,
+    {parse_mode : "HTML"})
+  }
+})
+/* /links Command */
+bot.command("links", async (ctx) => {
+  ctx.telegram.sendChatAction(ctx.chat.id, "typing")
+  let response = await fetch("https://eklavya.deta.dev/socials")
+  let data = await response.json()
+  ctx.reply(`<b>All Links</b>
+• Instagram : ${data.instagram}
+• Telegram : ${data.telegram}
+• Website : ${data.website}`,
+  {parse_mode : "HTML", disable_web_page_preview : true})
+})
